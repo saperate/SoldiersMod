47901028907bdfedfb74a6131fd2440a546a7142
saperate <61527642+saperate@users.noreply.github.com>
2023-07-21 16:58:21 -0400

    Basic Mod, nothing yet
    i went to https://fabricmc.net/wiki/tutorial:setup
    https://fabricmc.net/develop/template/
    download the template,
    open vscode
    let it run
    change the file names
    ./gradlew vscode
    ./gradlew gensources

--- src/client/java/saperate/soldiersmod/SoldiersModClient.java
+++ src/client/java/saperate/soldiersmod/SoldiersModClient.java
@@ -2,7 +2,7 @@ package saperate.soldiersmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class SoldiersModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/saperate/soldiersmod/SoldiersModDataGenerator.java
+++ src/main/java/saperate/soldiersmod/SoldiersModDataGenerator.java
@@ -3,7 +3,7 @@ package saperate.soldiersmod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class SoldiersModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
